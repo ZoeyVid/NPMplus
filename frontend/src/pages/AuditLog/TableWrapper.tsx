e40e46abb0e2c363d2fd17a731bd04329e42df6d
GitHub <noreply@github.com>
2025-12-03 04:47:26 +0000

    update and lint
    Signed-off-by: GitHub <noreply@github.com>

--- frontend/src/pages/AuditLog/TableWrapper.tsx
+++ frontend/src/pages/AuditLog/TableWrapper.tsx
@@ -13,11 +13,7 @@ export default function TableWrapper() {
 	}
 
 	if (isError) {
-		return (
-			<Alert variant="danger">
-				{error?.message || <T id="error.unknown" />}
-			</Alert>
-		);
+		return <Alert variant="danger">{error?.message || <T id="error.unknown" />}</Alert>;
 	}
 
 	return (
--- frontend/src/pages/Users/TableWrapper.tsx
+++ frontend/src/pages/Users/TableWrapper.tsx
@@ -23,11 +23,7 @@ export default function TableWrapper() {
 	}
 
 	if (isError) {
-		return (
-			<Alert variant="danger">
-				{error?.message || <T id="error.unknown" />}
-			</Alert>
-		);
+		return <Alert variant="danger">{error?.message || <T id="error.unknown" />}</Alert>;
 	}
 
 	const handleLoginAs = async (id: number) => {
