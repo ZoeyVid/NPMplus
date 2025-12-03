e8f177529147685aa869242596177335b5a3bdd1
GitHub <noreply@github.com>
2025-12-03 14:03:23 +0000

    update and lint
    Signed-off-by: GitHub <noreply@github.com>

--- frontend/src/components/AnimatedModalBody.tsx
+++ frontend/src/components/AnimatedModalBody.tsx
@@ -9,7 +9,13 @@ interface Props {
 
 export const AnimatedModalBody = ({ children, className }: Props) => {
 	return (
-		<Modal.Body className={className} as={motion.div} initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3 }}>
+		<Modal.Body
+			className={className}
+			as={motion.div}
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.3 }}
+		>
 			{children}
 		</Modal.Body>
 	);
