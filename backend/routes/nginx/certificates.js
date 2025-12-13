304af4db69e7df90742afb3e501e58529888ec19
GitHub <noreply@github.com>
2025-12-13 23:00:54 +0000

    update and lint
    Signed-off-by: GitHub <noreply@github.com>

--- backend/routes/nginx/certificates.js
+++ backend/routes/nginx/certificates.js
@@ -19,9 +19,8 @@ const downloadLimiter = rateLimit({
 	max: 10, // limit each IP to 10 download requests per windowMs
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-	message: { error: "Too many download requests from this IP, please try again later." }
+	message: { error: "Too many download requests from this IP, please try again later." },
 });
-
 
 /**
  * /api/nginx/certificates
