050d2ee5efb773f862cfb37b66c63111b6574954
유진 <79776214+yujin6121@users.noreply.github.com>
2025-12-14 13:39:43 +0000

    start

--- frontend/src/api/backend/getToken.ts
+++
@@ -1,9 +0,0 @@
-import * as api from "./base";
-import type { TokenResponse } from "./responseTypes";
-
-export async function getToken(identity: string, secret: string): Promise<TokenResponse> {
-	return await api.post({
-		url: "/tokens",
-		data: { identity, secret },
-	});
-}
