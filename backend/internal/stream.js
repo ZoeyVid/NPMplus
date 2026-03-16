288b4a59af5aa3cb130cbcca83c14b77b2195bc1
Zoey <zoey@z0ey.de>
2026-03-16 07:29:07 +0000

    Apply suggestion from @Zoey2936
    Signed-off-by: Zoey <zoey@z0ey.de>

--- backend/internal/stream.js
+++ backend/internal/stream.js
@@ -385,9 +385,9 @@ const internalStream = {
 				if (typeof search_query === "string" && search_query.length > 0) {
 					query.where(function () {
 						this.where(castJsonIfNeed("incoming_port"), "like", `%${search_query}%`)
-							.orWhere("npmplus_description", "like", `%${search_query}%`)
+							.orWhere(castJsonIfNeed("forwarding_port"), "like", `%${search_query}%`)
 							.orWhere("forwarding_host", "like", `%${search_query}%`)
-							.orWhere(castJsonIfNeed("forwarding_port"), "like", `%${search_query}%`);
+							.orWhere("npmplus_description", "like", `%${search_query}%`);
 					});
 				}
 
