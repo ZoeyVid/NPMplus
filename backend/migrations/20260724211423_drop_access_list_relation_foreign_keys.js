e4adcd7238e98a87ce0b24b888054483c7cacb9b
Zoey <zoey@z0ey.de>
2026-07-24 22:11:00 +0200

    add migrations to fix npmplus_proxy_host_access_list table

---
+++ backend/migrations/20260724211423_drop_access_list_relation_foreign_keys.js
@@ -0,0 +1,35 @@
+import { migrate as logger } from "../logger.js";
+
+const migrateName = "drop_access_list_relation_foreign_keys";
+
+/**
+ * Migrate
+ *
+ * @see https://knexjs.org/guide/migrations.html#migration-api
+ *
+ * @param   {Object} knex
+ * @returns {Promise}
+ */
+const up = async (knex) => {
+	logger.info(`[${migrateName}] Migrating Up...`);
+
+	await knex.schema.alterTable("npmplus_proxy_host_access_list", (table) => {
+		table.dropForeign("proxy_host_id");
+		table.dropForeign("access_list_id");
+	});
+
+	logger.info(`[${migrateName}] npmplus_proxy_host_access_list Table altered`);
+};
+
+/**
+ * Undo Migrate
+ *
+ * @param   {Object} _knex
+ * @returns {Promise}
+ */
+const down = (_knex) => {
+	logger.warn(`[${migrateName}] You can't migrate down this one.`);
+	return Promise.resolve(true);
+};
+
+export { down, up };
