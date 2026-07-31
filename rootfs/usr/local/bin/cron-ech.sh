9ed66d835e20e1116b019f70ce01016e6e2acc43
Zoey <zoey@z0ey.de>
2026-07-31 21:12:49 +0200

    improve ech scripts
    Signed-off-by: Zoey <zoey@z0ey.de>

--- rootfs/usr/local/bin/cron-ech.sh
+++ rootfs/usr/local/bin/cron-ech.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
 if [ -s /data/tls/ech/cron.sh ]; then
+    rm -f /data/tls/ech/*-previous.ech
+    jq '{current: [], previous: .current}' /data/tls/ech/config-ids.json | sponge /data/tls/ech/config-ids.json
     chmod +x /data/tls/ech/cron.sh
     /data/tls/ech/cron.sh
     sed -i "s|#ssl_ech_file|ssl_ech_file|g" /usr/local/nginx/conf/nginx.conf
     nginx -s reload
 elif grep -q '^[^#]*ssl_ech_file' /usr/local/nginx/conf/nginx.conf; then
+    rm -f /data/tls/ech/*.ech
+    jq -n '{current: [], previous: []}' | sponge /data/tls/ech/config-ids.json
     sed -i "s|ssl_ech_file|#ssl_ech_file|g" /usr/local/nginx/conf/nginx.conf
     nginx -s reload
 fi
