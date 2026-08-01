c05c2d66fdf9eed85bd737bb072905032fcbd888
Zoey <zoey@z0ey.de>
2026-08-01 19:51:48 +0200

    Update nginx-ech-boringssl-awslc.patch
    Signed-off-by: Zoey <zoey@z0ey.de>

--- rootfs/usr/local/bin/launch.sh
+++ rootfs/usr/local/bin/launch.sh
@@ -55,13 +55,19 @@ fi
 if [ -s /data/tls/ech/cron.sh ]; then
     rm -f /data/tls/ech/*-previous.ech
     jq '{current: [], previous: .current}' /data/tls/ech/config-ids.json | sponge /data/tls/ech/config-ids.json
+
     chmod +x /data/tls/ech/cron.sh
     /data/tls/ech/cron.sh
-    sed -i "s|#ssl_ech_file|ssl_ech_file|g" /usr/local/nginx/conf/nginx.conf
-elif grep -q '^[^#]*ssl_ech_file' /usr/local/nginx/conf/nginx.conf; then
+
+    : > /data/tls/ech/nginx.conf
+    for file in /data/tls/ech/*.ech; do
+        [ -s "$file" ] && echo "ssl_ech_file $file;" >> /data/tls/ech/nginx.conf
+    done
+elif [ -s /data/tls/ech/nginx.conf ]; then
     rm -f /data/tls/ech/*.ech
     jq -n '{current: [], previous: []}' | sponge /data/tls/ech/config-ids.json
-    sed -i "s|ssl_ech_file|#ssl_ech_file|g" /usr/local/nginx/conf/nginx.conf
+
+    : > /data/tls/ech/nginx.conf
 fi
 
 
