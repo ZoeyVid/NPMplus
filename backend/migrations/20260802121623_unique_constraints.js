fa94d888d1d3898c8915df05bf62d69911952d8e
Zoey <zoey@z0ey.de>
2026-08-02 12:40:06 +0200

    add unique to auth table

---
+++ backend/migrations/20260802121623_unique_constraints.js
@@ -0,0 +1,40 @@
+import { migrate as logger } from "../logger.js";
+
+const migrateName = "unique_constraints";
+
+/**
+ * Migrate
+ *
+ * @see https://knexjs.org/guide/migrations.html#migration-api
+ *
+ * @param   {Object} knex
+ * @returns {Promise}
+ */
+const up = async (knex) => {
+	logger.info(`[${migrateName}] Migrating Up...`);
+
+	await knex("auth")
+		.whereNotIn("id", knex.select("id").from(knex("auth").min("id as id").groupBy(["user_id", "type"]).as("keep")))
+		.delete();
+
+	logger.info(`[${migrateName}] Duplicates removed`);
+
+	await knex.schema.alterTable("auth", (table) => {
+		table.unique(["user_id", "type"]);
+	});
+
+	logger.info(`[${migrateName}] auth Table altered`);
+};
+
+/**
+ * Undo Migrate
+ *
+ * @param   {Object} _knex
+ * @returns {Promise}
+ */
+const down = (_knex) => {
+	logger.warn(`[${migrateName}] You can't migrate down this one.`);
+	return Promise.resolve(true);
+};
+
+export { down, up };
