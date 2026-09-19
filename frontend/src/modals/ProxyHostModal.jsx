17da7d551addb0f4bbf04c2d5ab1f4ae4eeede4f
Zoey <zoey@z0ey.de>
2026-09-20 00:35:04 +0200

    remove unused data-bs-dismiss
    Signed-off-by: Zoey <zoey@z0ey.de>

--- frontend/src/modals/ProxyHostModal.jsx
+++ frontend/src/modals/ProxyHostModal.jsx
@@ -790,7 +790,7 @@ const ProxyHostModal = EasyModal.create(({ id, isClone = false, visible, remove 
 								</div>
 							</Modal.Body>
 							<Modal.Footer>
-								<Button data-bs-dismiss="modal" onClick={remove} disabled={isSubmitting}>
+								<Button onClick={remove} disabled={isSubmitting}>
 									<T id="cancel" />
 								</Button>
 								<HasPermission section={PROXY_HOSTS} permission={MANAGE} hideError>
@@ -798,7 +798,6 @@ const ProxyHostModal = EasyModal.create(({ id, isClone = false, visible, remove 
 										type="submit"
 										actionType="primary"
 										className="ms-auto bg-lime"
-										data-bs-dismiss="modal"
 										isLoading={isSubmitting}
 										disabled={isSubmitting}
 									>
