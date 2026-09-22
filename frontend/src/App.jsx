cb75aafd2294432ddc50aaa66b3d671aa1fe8046
Zoey <zoey@z0ey.de>
2026-09-22 21:56:51 +0200

    further simplify locale code
    Signed-off-by: Zoey <zoey@z0ey.de>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { RawIntlProvider } from "react-intl";
 import { ToastContainer } from "react-toastify";
 import { queryClient } from "src/api/backend/base";
-import { AuthProvider, LocaleProvider, ThemeProvider } from "src/context";
+import { AuthProvider, ThemeProvider } from "src/context";
 import { intl } from "src/locale";
 import EasyModal from "src/modules/easyModal";
 import Router from "src/Router.jsx";
@@ -10,26 +10,24 @@ import Router from "src/Router.jsx";
 function App() {
 	return (
 		<RawIntlProvider value={intl}>
-			<LocaleProvider>
-				<ThemeProvider>
-					<QueryClientProvider client={queryClient}>
-						<AuthProvider>
-							<EasyModal.Provider>
-								<Router />
-							</EasyModal.Provider>
-							<ToastContainer
-								position="top-right"
-								autoClose={5000}
-								hideProgressBar={true}
-								newestOnTop={true}
-								closeOnClick={true}
-								rtl={document.dir === "rtl"}
-								closeButton={false}
-							/>
-						</AuthProvider>
-					</QueryClientProvider>
-				</ThemeProvider>
-			</LocaleProvider>
+			<ThemeProvider>
+				<QueryClientProvider client={queryClient}>
+					<AuthProvider>
+						<EasyModal.Provider>
+							<Router />
+						</EasyModal.Provider>
+						<ToastContainer
+							position="top-right"
+							autoClose={5000}
+							hideProgressBar={true}
+							newestOnTop={true}
+							closeOnClick={true}
+							rtl={document.dir === "rtl"}
+							closeButton={false}
+						/>
+					</AuthProvider>
+				</QueryClientProvider>
+			</ThemeProvider>
 		</RawIntlProvider>
 	);
 }
